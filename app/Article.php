31188405fd2fb6c75249a18a73f9925d16a8a0a5
unknown <emmanuel.dave02@gmail.com>
2021-02-10 12:41:35 +0700

    Database Config

---
+++ app/Article.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Article extends Model
+{
+    protected $fillable = ['title', 'description', 'image'];
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+
+    public function category(){
+        return $this->belongsTo(Category::class);
+    }
+}
