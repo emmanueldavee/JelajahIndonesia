289b65be957f30b93b7cf4ee5f13a3bc1e5192a1
unknown <emmanuel.dave02@gmail.com>
2021-02-10 23:03:24 +0700

    Remove Route

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -67,11 +67,9 @@
                             <li class="nav-item">
                                 <a class="nav-link" href="{{ url('/login') }}">{{ __('Login') }}</a>
                             </li>
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ url('/register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ url('/register') }}">{{ __('Register') }}</a>
+                            </li>
                         @else
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
